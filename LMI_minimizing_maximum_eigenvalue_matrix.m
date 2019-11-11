4153ba8b5b37d4ad37754a8d9e352d9bb8760c19
asalimil <43479233+asalimil@users.noreply.github.com>
2019-11-11 11:48:08 -0700

    Create LMI_minimizing_maximum_eigenvalue_matrix.m

---
+++ LMI_minimizing_maximum_eigenvalue_matrix.m
@@ -0,0 +1,32 @@
+clc; clear; close all;
+
+% Variables
+t = sdpvar(1);
+x1 = sdpvar(1); 
+x2 = sdpvar(1);
+
+% Ai (i = 1,2, ..., n) are symmetric matrices
+A0 = [1  2 
+      2  4];
+A1 = [5   3
+      3  -2];
+A2 = [1   0
+      0   4];
+
+% LMI
+eta = 0.00000001;
+C = [t >= eta];
+A = A0 + A1*x1 + A2*x2;
+F = [C, A - t*eye(2) <= 0];
+optimize(F,t);
+
+% maximum eigenvalues
+t = value(t)
+
+% xi
+x1 = value(x1)
+x2 = value(x2)
+
+% resulted A matrix
+A = A0 + A1*x1 + A2*x2;
+eigenvalues = eig(A)
